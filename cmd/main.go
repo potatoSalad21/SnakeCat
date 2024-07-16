0a372439ecf357e4656fd20ac8d58cd4976d4d38
potatoSalad21 <datuna.ziraqishvili@gmail.com>
2024-07-16 14:48:30 +0400

    loading textures

--- cmd/main.go
+++ cmd/main.go
@@ -1,68 +1,66 @@
 package main
 
 import (
-    "fmt"
+	"fmt"
 
-    rl "github.com/gen2brain/raylib-go/raylib"
+	rl "github.com/gen2brain/raylib-go/raylib"
 )
 
 const (
-    screenWidth = 720
-    screenHeight = 480
+	screenWidth  = 720
+	screenHeight = 480
+    vel = 10.0
 )
 
 type Cat struct {
-    posX int32
-    posY int32
+	posX    float32
+	posY    float32
+	src     rl.Rectangle
+	dest    rl.Rectangle
+	texture rl.Texture2D
 }
 
-func input(cat *Cat) {
-    if rl.IsKeyDown(rl.KeyW) {
-        cat.posY -= 10
-    }
-    if rl.IsKeyDown(rl.KeyS) {
-        cat.posY += 10
-    }
-    if rl.IsKeyDown(rl.KeyA) {
-        cat.posX -= 10
-    }
-    if rl.IsKeyDown(rl.KeyD) {
-        cat.posX += 10
-    }
-}
-
-func update() {
-
-}
-
-func drawScene() {
-
+func input(c *Cat) {
+	if rl.IsKeyDown(rl.KeyW) {
+		c.dest.Y -= vel
+	}
+	if rl.IsKeyDown(rl.KeyS) {
+		c.dest.Y += vel
+	}
+	if rl.IsKeyDown(rl.KeyA) {
+		c.dest.X -= vel
+	}
+	if rl.IsKeyDown(rl.KeyD) {
+		c.dest.X += vel
+	}
 }
 
 func render(c *Cat) {
-    rl.BeginDrawing()
+	rl.BeginDrawing()
 
-    // draw background
-    rl.ClearBackground(rl.NewColor(147, 211, 196, 255))
+	rl.ClearBackground(rl.NewColor(147, 211, 196, 255))
+	rl.DrawTexturePro(c.texture, c.src, c.dest, rl.NewVector2(c.dest.Width, c.dest.Height), 0, rl.White)
 
-    rl.DrawRectangle(c.posX, c.posY, 64, 64, rl.NewColor(255, 0, 0, 255))
-
-    rl.EndDrawing()
+	rl.EndDrawing()
 }
 
 func main() {
-    fmt.Println("Peak gameplay")
-    rl.InitWindow(screenWidth, screenHeight, "DEMO")
-    defer rl.CloseWindow()
+	fmt.Println("Peak gameplay")
+	rl.InitWindow(screenWidth, screenHeight, "DEMO")
+	defer rl.CloseWindow()
 
-    rl.SetExitKey(0)
-    rl.SetTargetFPS(60)
+	rl.SetExitKey(0)
+	rl.SetTargetFPS(60)
 
-    cat := new(Cat)
+	cat := new(Cat)
+	cat.texture = rl.LoadTexture("./assets/Block.png")
+	defer rl.UnloadTexture(cat.texture)
 
-    for !rl.WindowShouldClose() {
-        input(cat)
-        update()
-        render(cat)
-    }
+	cat.src = rl.NewRectangle(0, 0, 40, 40)
+	cat.dest = rl.NewRectangle(200, 200, 48, 48)
+
+	for !rl.WindowShouldClose() {
+		input(cat)
+		render(cat)
+	}
 }
