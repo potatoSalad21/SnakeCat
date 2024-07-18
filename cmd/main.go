0053af942b9236e366591e161149aea7c4146484
potatoSalad21 <datuna.ziraqishvili@gmail.com>
2024-07-18 12:06:11 +0400

    FEAT: snake-ish movement

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
     "math/rand/v2"
+    "time"
 
 	rl "github.com/gen2brain/raylib-go/raylib"
 )
@@ -12,8 +13,8 @@ var tileDest rl.Rectangle
 const (
 	screenWidth  = 960
 	screenHeight = 960
-	tileSize     = 32
-	tileNum      = 30
+	tileSize     = 48
+	tileNum      = 20
 	vel          = 5.0
 )
 
@@ -34,6 +35,21 @@ type Food struct {
 	texture rl.Texture2D
 }
 
+func (c *Cat) move() {
+    dir := c.direction
+
+    switch {
+    case dir.X == 1 && dir.Y == 0:
+        c.dest.X += tileSize
+    case dir.X == -1 && dir.Y == 0:
+        c.dest.X -= tileSize
+    case dir.X == 0 && dir.Y == 1:
+        c.dest.Y += tileSize
+    case dir.X == 0 && dir.Y == -1:
+        c.dest.Y -= tileSize
+    }
+}
+
 func handleMovement(c *Cat) {
 	if rl.IsKeyDown(rl.KeyW) {
 		c.direction.X = 0
@@ -51,17 +67,14 @@ func handleMovement(c *Cat) {
 		c.direction.X = 1
 		c.direction.Y = 0
 	}
-
-	c.dest.X += c.direction.X * vel
-	c.dest.Y += c.direction.Y * vel
 }
 
 func spawnFood(f *Food) {
-    row := rand.IntN(31)
-    col := rand.IntN(31)
+    row := rand.IntN(tileNum)
+    col := rand.IntN(tileNum)
 
-    f.dest.X = float32(row) * f.dest.Width
-    f.dest.Y = float32(col) * f.dest.Height
+    f.dest.X = float32(row) * tileSize
+    f.dest.Y = float32(col) * tileSize
 }
 
 func render(c *Cat, food *Food, grassSprite rl.Texture2D, tileSrc rl.Rectangle) {
@@ -98,20 +111,34 @@ func main() {
 	cat := new(Cat)
 	cat.direction = rl.Vector2{X: 1, Y: 0} // default direction: right
 	cat.src = rl.NewRectangle(0, 0, 40, 40)
-	cat.dest = rl.NewRectangle(200, 200, 48, 48)
+	cat.dest = rl.NewRectangle(tileSize * (tileNum / 2), tileSize * (tileNum / 2), 48, 48)
 	cat.texture = rl.LoadTexture("./assets/Block.png")
 	defer rl.UnloadTexture(cat.texture)
 
 	food := new(Food)
 	food.src = rl.NewRectangle(0, 0, 48, 32)
-    food.dest.Width = 48
-    food.dest.Height = 32
+    food.dest.Width = tileSize
+    food.dest.Height = tileSize
 	food.texture = rl.LoadTexture("./assets/fish.png")
 	defer rl.UnloadTexture(food.texture)
+
+    ticker := time.NewTicker(200 * time.Millisecond)
+    done := make(chan bool)
+    go func() {
+        for {
+            select {
+            case <- done:
+                return
+            case <- ticker.C:
+                cat.move()
+            }
+        }
+    }()
 
     spawnFood(food)
 	for !rl.WindowShouldClose() {
 		handleMovement(cat)
 		render(cat, food, grassSprite, tileSrc)
 	}
+    ticker.Stop()
 }
